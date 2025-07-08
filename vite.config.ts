fe9ecf0f26f68e18424ddd5f05363fe2cff69829
Zach Blake <zblake@nrttech.com>
2025-07-07 18:51:38 -0700

    something im sure

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		allowedHosts: ['localhost', 'dod.ztb.dev'],
-		origin: 'https://dod.ztb.dev'
-	}
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+        allowedHosts: ['localhost', 'dod.ztb.dev'],
+        origin: 'https://dod.ztb.dev'
+    }
 });
